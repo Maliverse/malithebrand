291b952e2e8aaa7cbe8b70a6bbcf5b3b932c9ec0
maliverse <amaliakwunte@gmail.com>
2022-04-14 17:57:06 +0100

    first commit

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import './header.css';
+
+
+const Header = () => {
+
+    return (
+        <div className='malithebrand__header-container  '>
+            <div className='malithebrand__header-top'>
+            <div className='malithebrand__header-title'>
+                <h1 className='malithebrand__header-name tracking-in-contract-bck'>akwunté</h1>
+                <p>DESIGNER X DEVELOPER</p>
+            </div>
+            <div className='malithebrand__button'>
+            <button className='jello-horizontal'>View portfolio</button>
+            </div>
+            </div>
+          
+        </div>
+    )
+}
+
+export default Header
