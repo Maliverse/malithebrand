c1948dd2058dca0578cfa2847e76bb8928051e07
maliverse <amaliakwunte@gmail.com>
2022-04-27 19:37:54 +0100

    converted hex to filter and added hover to logo

--- src/containers/projects/Projects.jsx
+++ src/containers/projects/Projects.jsx
@@ -5,16 +5,16 @@ const Projects = () => {
     return(
         <div className="project__container">
 <div className="project__title">
-        <h1>Some Projects <br /> We've Worked On</h1>
+        <h1>Some <br /> interesting projects</h1>
  </div>
 
         <div className="project__cards">
-            <div className="card 1">Project</div>
-            <div className="card 2">Project</div>
-            <div className="card 3">Project</div>
-            <div className="card 4">Project</div>
-            <div className="card 5">Project</div>
-            <div className="card 6">Project</div>
+            <div className="card 1">1</div>
+            <div className="card 2">2</div>
+            <div className="card 3">3</div>
+            <div className="card 4">4</div>
+            <div className="card 5">5</div>
+            <div className="card 6">6</div>
         </div>
 
         </div>  
