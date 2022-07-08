9c14b629783a5e392518db2e5f02d4fe5a5bd378
maliverse <amaliakwunte@gmail.com>
2022-07-08 16:11:40 +0100

    removed project boxes

--- src/containers/projects/Projects.jsx
+++ src/containers/projects/Projects.jsx
@@ -67,54 +67,9 @@ const Projects = () => {
 
 <div className="card_title-block"> Nexus Humanitarian </div>
             </div>
-            <div className="card four">
-
-            <div className="card-frame"></div>
-
-<div className="card_icon-box">
-    <div className="card_icon">
-    <FiFigma color="orange" size={30}/>
-    </div>
-    <div className="card_icon">
-    <FiLink color="orange" size={30} />
-    </div>
-</div>
-
-
-<div className="card_title-block"> Project </div>
-            </div>
-            <div className="card five">
-
-            <div className="card-frame"></div>
-
-<div className="card_icon-box">
-    <div className="card_icon">
-    <FiFigma color="orange" size={30}/>
-    </div>
-    <div className="card_icon">
-    <FiLink color="orange" size={30} />
-    </div>
-</div>
-
-{/* 
-<div className="card_title-block"> Project </div>
-            </div>
-            <div className="card six">
-
-            <div className="card-frame"></div>
-
-<div className="card_icon-box">
-    <div className="card_icon">
-    <FiFigma color="orange" size={30}/>
-    </div>
-    <div className="card_icon">
-    <FiLink color="orange" size={30} />
-    </div>
-</div>
-*/}
-
-<div className="card_title-block"> Project </div>
-            </div>
+          
+          
+  
         </div>
 
         </div>  
