31b68868758f328d68d0f2329a083304cd368ad5
maliverse <amaliakwunte@gmail.com>
2022-07-14 12:22:08 +0100

    added back fonts

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,3 @@
-export { default as Anime } from './anime/Anime';
-
 export { default as About } from './about/About';
 export { default as Brands } from './brands/Brands';
 export { default as Reviews} from '../containers/reviews/Reviews';
