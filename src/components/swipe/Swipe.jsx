665390a59a497d71872f36ae0cef38587f0de34e
maliverse <amaliakwunte@gmail.com>
2022-07-22 17:53:15 +0100

    added more reviews

--- src/components/swipe/Swipe.jsx
+++ src/components/swipe/Swipe.jsx
@@ -24,48 +24,55 @@ export default function Swipe() {
         <SwiperSlide>
 
          <h1 className="swiper-text">
-         Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, dolorum beatae.
+         A creative mind set to do great things.
           </h1> 
     
           <div className="swiper-img a"></div>
-          <h2>Dr. Karis E. Ameh-Wealth</h2>
+          <h2>Dr. Karis E.</h2>
+          <h3>President - Gloryzone Global</h3>
           </SwiperSlide>
 
         <SwiperSlide>
+
         <h1 className="swiper-text">
-        Working with Mali is amazing!  he executes every idea to perfection. He takes his time and he knows when something can be better. A great mind.
+        Working with Mali is amazing! Pun intended. He executes every idea to perfection. He takes his time and he knows when something can be better. A great mind.
           </h1> 
           <div className="swiper-img b"></div>
           <h2>Sesmo</h2>
+          <h3>Creative Director</h3>
         </SwiperSlide>
 
         <SwiperSlide>
         <h1 className="swiper-text">
-         Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, dolorum beatae.
+         Akwunte is definitely someone you would want to handle your projects. He delivers excellently.
           </h1> 
           <div className="swiper-img c"></div>
-          <h2>Emmanuel Werna</h2>
+          <h2>Emmanuel</h2>
+          <h3>Data Analyst</h3>
         </SwiperSlide>
 
         <SwiperSlide>
         <h1 className="swiper-text">
-         Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, dolorum beatae.
+         He never compromises on his standards, work ethic and in his character, and always strives to take giant strides in his career.
           </h1> 
           <div className="swiper-img d"></div>
-          <h2>Dooyum Abaa</h2>
+          <h2>Ramatu</h2>
+          <h3>Lawyer</h3>
         </SwiperSlide>
 
         <SwiperSlide>
         <h1 className="swiper-text">
-         Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, dolorum beatae.
+        An excellent creative.
           </h1> 
-          <div className="swiper-img"></div>
-          <h2>Ebenezer Adedayor</h2>
+          <div className="swiper-img e"></div>
+          <h2>Ebenezer</h2>
+          <h3>Fashion Designer</h3>
         </SwiperSlide>
         {/* <SwiperSlide>Slide 6</SwiperSlide>
         <SwiperSlide>Slide 7</SwiperSlide>
         <SwiperSlide>Slide 8</SwiperSlide>
         <SwiperSlide>Slide 9</SwiperSlide> */}
+
       </Swiper>
     </>
 
